094703059d18de24989e02f6697bf60b1e8acef1
uno <st9007a@gmail.com>
2017-10-11 16:17:23 +0800

    Change test method of bench test
    Old method use all 3-char word to test prefix search.
    New method use a dictionary to test prefix search.

--- bench.c
+++ bench.c
@@ -3,9 +3,8 @@
 #include <time.h>
 
 #include "bench.h"
-
-static const char *upper_case_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-static const char *lower_case_alphabet = "abcdefghijklmnopqrstuvwxyz";
+#define DICT_FILE "prefix_test.txt"
+#define WORDMAX 256
 
 double tvgetf()
 {
@@ -22,32 +21,38 @@ double tvgetf()
 
 int bench_test(const tst_node *root, char *out_file, const int max)
 {
-    char prefix[3];
+    char prefix[3] = "";
+    char word[WORDMAX] = "";
     char **sgl;
     FILE *fp = fopen(out_file, "w");
+    FILE *dict = fopen(DICT_FILE, "r");
     int idx = 0, sidx = 0;
     double t1, t2;
 
-    if (!fp) {
-        fprintf(stderr, "error: file open failed '%s'.\n", out_file);
+    if (!fp || !dict) {
+        if (fp) {
+            fprintf(stderr, "error: file open failed in '%s'.\n", DICT_FILE);
+            fclose(fp);
+        }
+        if (dict) {
+            fprintf(stderr, "error: file open failed in '%s'.\n", out_file);
+            fclose(dict);
+        }
         return 1;
     }
 
     sgl = (char **)malloc(sizeof(char *) * max);
-    for (int i = 0; i < 26; i++) {
-        prefix[0] = upper_case_alphabet[i];
-        for (int j = 0; j < 26; j++) {
-            prefix[1] = lower_case_alphabet[j];
-            for (int k = 0; k < 26; k++) {
-                prefix[2] = lower_case_alphabet[k];
-                t1 = tvgetf();
-                tst_search_prefix(root, prefix, sgl, &sidx, max);
-                t2 = tvgetf();
-                fprintf(fp, "%d %.6f sec\n", idx, t2 - t1);
-                idx++;
-            }
-        }
+    while (fscanf(dict, "%s", word) != EOF) {
+        if (strlen(word) < 4) continue;
+        strncpy(prefix, word, 3);
+        t1 = tvgetf();
+        tst_search_prefix(root, prefix, sgl, &sidx, max);
+        t2 = tvgetf();
+        fprintf(fp, "%d %.6f sec\n", idx, t2 - t1);
+        idx++;
     }
+
     fclose(fp);
+    fclose(dict);
     return 0;
 }
